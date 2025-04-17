15bfd782ba321e49aa130c2817da48ffdace32e9
Dev-KimDonghyun <hyungus723@gmail.com>
2025-04-17 10:02:48 +0900

    Refactor: Refactoring to TypeScript Style

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,13 +3,13 @@ import './../styles/index.css';
 
 const MainPage = () => {
 
-    const [ test, setTest ] = useState('');
+    const [ test, setTest ] = useState<string>('');
 
     useEffect(() => {
         localStorage.setItem('DevKim', '테스트 값');
-        const storageValue = localStorage.getItem('DevKim');
+        const storageValue: string | null = localStorage.getItem('DevKim');
         console.log('첫번째 console.log', storageValue);
-        if (storageValue) {
+        if (storageValue !== null) {
             setTest(storageValue);
         }
     }, []); // []가 비어있으므로 처음 화면에 나타났을때만 실행되는 코드인거임

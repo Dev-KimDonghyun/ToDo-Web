2b45697c0d5c9ec7ce0c83a30433724033a495a1
Dev-KimDonghyun <hyungus723@gmail.com>
2025-04-19 05:56:22 +0900

    Feat: On App.tsx, Add logic to use useState to handle user input and store data in localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,48 @@
-import DoInput from './components/DoInput';
-import DoList from './components/DoList';
+import { useEffect, useState } from "react";
 
 function App () {
+
+  const [ toDoList, setToDoList ] = useState<string[]>([]);
+  const [ toDoInput, setToDoInput ] = useState<string>('');
+
+  useEffect(() => {
+    const localStorageValue: string | null = localStorage.getItem('DevKimDH_To_Do_Web_Value');
+    if (localStorageValue !== null) {
+      const savedValue = JSON.parse(localStorageValue);
+      setToDoList(savedValue);
+    }
+  }, []);
+
+  const uploadToList = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const newList = [...toDoList, toDoInput];
+    setToDoInput('');
+    setToDoList(newList);
+    localStorage.setItem(('DevKimDH_To_Do_Web_Value'), JSON.stringify(newList));
+  }
+
   return (
     <div>
       <div>
+        <form onSubmit={uploadToList}>
+          <input
+          onChange={(e) => {
+            setToDoInput(e.target.value)
+            console.log(e.target.value)
+          }} // e.target == <input/>
+          value={toDoInput}
+          required>
+          </input>
+          <button
+          type='submit'>
+            버튼임
+          </button>
+        </form>
         <h1>테스트</h1>
-        <DoInput />
-        <DoList />
       </div>
     </div>
   );
+
 }
 
 export default App;
--- src/components/DoInput.tsx
+++ src/components/DoInput.tsx
@@ -1,27 +1,7 @@
-import { useEffect, useState } from "react";
-
 const DoInput = () => {
-
-  const [ toDoInput, setToDoInput ] = useState<string>('');
-
-  useEffect(() => {
-    const localStorageValue: string | null = localStorage.getItem('DevKimDH_To_Do_Web_Value');
-    if (localStorageValue !== null) {
-      const localStorageJSONValue = JSON.parse(localStorageValue); // 문자열을 객체로 변환
-      setToDoInput(localStorageJSONValue); 
-    }
-  }, []);
-
-  const addToDoList = () => {
-    //
-  }
-
   return (
-
     <div>DoInput</div>
-
   );
-
 }
 
 export default DoInput;
